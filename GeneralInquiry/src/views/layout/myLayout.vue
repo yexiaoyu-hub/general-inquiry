3f71145e04ee6277b0b923c8c4113ae657985bbb
lao-guo18589921358 <396728672@qq.com>
2026-03-17 09:02:53 +0800

    测试提交

--- GeneralInquiry/src/views/layout/myLayout.vue
+++ GeneralInquiry/src/views/layout/myLayout.vue
@@ -1,6 +1,12 @@
 <template>
       <div>
         助手布局
+        <el-menu :default-active="activeIndex" class="el-menu-demo" @select="handleSelect">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">咨询</el-menu-item>
+          <el-menu-item index="3">订单</el-menu-item>
+          <el-menu-item index="4">我的</el-menu-item>
+        </el-menu>
         <router-view></router-view>
       </div>
         
